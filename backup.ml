d0abd7ae445d7166c0363247185eda90d20e3744
Dylan Irlbeck <dylan@flexport.com>
2022-04-05 19:50:15 -0500

    Init basic networking setup

--- backup.ml
+++ backup.ml
@@ -1,3 +1,7 @@
+open Lwt
+open Cohttp
+open Cohttp_lwt_unix
+
 (* Notion API configuration: API url, export filename, Notion token v2, Notion
     space ID.
 
@@ -20,3 +24,16 @@ let config =
 ;;
 
 print_endline config.token_v2
+
+let body =
+  Client.get (Uri.of_string config.api) >>= fun (resp, body) ->
+  let code = resp |> Response.status |> Code.code_of_status in
+  Printf.printf "Response code: %d\n" code;
+  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
+  body |> Cohttp_lwt.Body.to_string >|= fun body ->
+  Printf.printf "Body of length: %d\n" (String.length body);
+  body
+
+let () =
+  let body = Lwt_main.run body in
+  print_endline ("Received body\n" ^ body)
