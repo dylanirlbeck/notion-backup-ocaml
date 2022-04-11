8ba95efc1e55a4d6ce6621dd031843273a10092d
Dylan Irlbeck <dylan@flexport.com>
2022-04-11 10:34:17 -0500

    Making progress (reword this message)

--- backup.ml
+++ backup.ml
@@ -21,19 +21,38 @@ let config =
     token_v2 = Sys.getenv "NOTION_TOKEN_V2";
     space_id = Sys.getenv "NOTION_SPACE_ID";
   }
+
+let headers =
+  let h = Header.init () in
+  let h = Header.add h "content-type" "application/json" in
+  Header.add h "cookie" (Printf.sprintf "token_v2 = %s; " config.token_v2)
+
+let enqueue_task_param =
+  {|
+  {"task" : {
+    "eventName": "exportSpace",
+    "request": {
+      "spaceId": config.space_id
+    }
+  }}
+|}
 ;;
 
-print_endline config.token_v2
+print_endline enqueue_task_param
 
-let body =
-  Client.get (Uri.of_string config.api) >>= fun (resp, body) ->
-  let code = resp |> Response.status |> Code.code_of_status in
-  Printf.printf "Response code: %d\n" code;
-  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
+let request endpoint ~params =
+  Printf.printf "Params: %d\n" params;
+  Client.get ~headers (Uri.of_string (config.api ^ endpoint))
+  >>= fun (_, body) ->
   body |> Cohttp_lwt.Body.to_string >|= fun body ->
-  Printf.printf "Body of length: %d\n" (String.length body);
+  (*
+    let code = resp |> Response.status |> Code.code_of_status in
+    Printf.printf "Response code: %d\n" code;
+    Printf.printf "Headers: %s\n" (resp |> Response.headers |>
+    Header.to_string);
+  *)
   body
 
 let () =
-  let body = Lwt_main.run body in
+  let body = Lwt_main.run (request "enqueueTask" ~params:0) in
   print_endline ("Received body\n" ^ body)
