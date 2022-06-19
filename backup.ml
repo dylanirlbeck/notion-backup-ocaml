b40c700188c444ce6d1e1a58a6a6dd78ce4436f5
Dylan Irlbeck <dylan@flexport.com>
2022-06-18 21:54:52 -0500

    MVP

--- backup.ml
+++ backup.ml
@@ -1,6 +1,7 @@
 open Lwt
 open Cohttp
 open Cohttp_lwt_unix
+open Yojson.Basic.Util
 
 (* Notion API configuration: API url, export filename, Notion token v2, Notion
     space ID.
@@ -18,7 +19,7 @@ module Config = struct
   let create =
     {
       api = "https://www.notion.so/api/v3";
-      export_filename = "export.zip";
+      export_filename = "notion_backup.zip";
       token_v2 = Sys.getenv "NOTION_TOKEN_V2";
       space_id = Sys.getenv "NOTION_SPACE_ID";
     }
@@ -29,7 +30,7 @@ let config = Config.create
 let headers =
   let h = Header.init () in
   let h = Header.add h "content-type" "application/json" in
-  Header.add h "cookie" (Printf.sprintf "token_v2 = %s; " config.token_v2)
+  Header.add h "cookie" (Printf.sprintf "token_v2=%s; " config.token_v2)
 
 let enqueue_task_param : Yojson.t =
   `Assoc
@@ -42,11 +43,11 @@ let enqueue_task_param : Yojson.t =
               `Assoc
                 [
                   ("spaceId", `String config.space_id);
-                  ( "exportType",
+                  ( "exportOptions",
                     `Assoc
                       [
                         ("exportType", `String "markdown");
-                        ("timeZone", `String "Europe/Amsterdam");
+                        ("timeZone", `String "America/Chicago");
                         ("locale", `String "en");
                       ] );
                 ] );
@@ -54,19 +55,65 @@ let enqueue_task_param : Yojson.t =
     ]
 
 let request endpoint ~params =
-  Yojson.pretty_to_channel stdout params;
   let body_string = Yojson.to_string params in
   Client.post
     ~body:(Cohttp_lwt.Body.of_string body_string)
     ~headers
     (Uri.of_string (config.api ^ endpoint))
-  >>= fun (resp, body) ->
-  body |> Cohttp_lwt.Body.to_string >|= fun body ->
-  let code = resp |> Response.status |> Code.code_of_status in
-  Printf.printf "Response code: %d\n" code;
-  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
-  body
+  >>= fun (_, body) ->
+  body |> Cohttp_lwt.Body.to_string >|= fun body -> body
+
+let construct_task_ids task_id : Yojson.t =
+  `Assoc [ ("taskIds", `List [ `String task_id ]) ]
+
+(* let print_tasks tasks = *)
+(*   List.iter *)
+(*     (fun task -> *)
+(*       task |> member "id" |> to_string |> Printf.printf "Fetched task %s\n") *)
+(*     tasks *)
+
+let rec request_tasks task_id =
+  Unix.sleep 2;
+  let task_ids_param = construct_task_ids task_id in
+  let tasks =
+    Yojson.Basic.from_string
+      (Lwt_main.run (request "/getTasks" ~params:task_ids_param))
+    |> member "results" |> to_list
+  in
+  let task =
+    List.find (fun task -> member "id" task |> to_string = task_id) tasks
+  in
+  let pages_exported =
+    task |> member "status" |> member "pagesExported" |> to_int
+  in
+  Printf.printf "\rPages exported: %i" pages_exported;
+  let state = task |> member "state" |> to_string in
+  match state with "success" -> task | _ -> request_tasks task_id
+
+(*Source: https://discuss.ocaml.org/t/how-do-i-download-a-file-with-cohttp/7105*)
+(* let download (uri : Uri.t) (dest : string) = *)
+(*   Client.get uri >>= fun (_, body) -> *)
+(*   (1* TODO: check response for status codes, follow redirects if applicable *1) *)
+(*   let stream = Cohttp_lwt.Body.to_stream body in *)
+(*   Lwt_io.with_file ~mode:Lwt_io.output dest (fun chan -> *)
+(*       Lwt_stream.iter_s (Lwt_io.write chan) stream) *)
 
 let () =
-  let body = Lwt_main.run (request "enqueueTask" ~params:enqueue_task_param) in
-  print_endline ("Received body\n" ^ body)
+  let task_id =
+    Yojson.Basic.from_string
+      (Lwt_main.run (request "/enqueueTask" ~params:enqueue_task_param))
+    |> member "taskId" |> to_string
+  in
+  Printf.printf "Enqueued task %s\n" task_id;
+  let ret_task = request_tasks task_id in
+  let export_url =
+    ret_task |> member "status" |> member "exportURL" |> to_string
+  in
+  Printf.printf "\nExport created, downloading: %s" export_url;
+  (* let _ = download (Uri.of_string export_url) config.export_filename in *)
+  let _ =
+    Unix.create_process "curl"
+      [| "curl"; export_url; "-o"; config.export_filename |]
+      Unix.stdin Unix.stdout Unix.stderr
+  in
+  Printf.printf "\nDownload complete: %s" config.export_filename
